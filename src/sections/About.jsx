bce078c749b3590abe2e8d6284703d225ea74d44
Pawan-Khushraho <pawankhushrahoofficial@gmail.com>
2024-11-24 00:57:17 +0530

    tech stack logo changed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
 
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain"/>
+                        <img src="assets/Typescript_logo_2020.svg.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain"/>
 
                         <div>
                             <p className="grid-headtext">Tech Stack</p>
