a2a59a59d34bd0b5f3da3372021555e542ad4ea8
Pawan-Khushraho <pawankhushrahoofficial@gmail.com>
2024-11-24 00:41:53 +0530

    navbar fixed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
     const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
     return (
-        <section className="min-h-screen w-full flex flex-col relative">
+        <section  id="home" className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
                 <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am
                     Pawan <span className="waving-hand">👋</span></p>
