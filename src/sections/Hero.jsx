aac8bdfec124fb158d4aaadae991ed7d4dc32d53
Pawan-Khushraho <pawankhushrahoofficial@gmail.com>
2024-11-24 00:42:56 +0530

    hero description changed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,7 +25,7 @@ const HeroSection = () => {
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
                 <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am
                     Pawan <span className="waving-hand">👋</span></p>
-                <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+                <p className="hero_tag text-gray_gradient">Software Engineer</p>
             </div>
             <div className="w-full h-full absolute inset-0">
                 <Leva/>
